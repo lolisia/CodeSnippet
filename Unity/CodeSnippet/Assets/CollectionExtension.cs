8f0c723a76f078cee8b9e87b9bc9fd5b89385320
lolisia <lolisia@gmail.com>
2016-12-22 18:50:56 +0900

    파일 위치 변경

---
+++ Unity/CodeSnippet/Assets/CollectionExtension.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace aisilol
+{
+	public static class DictionaryExtension
+	{
+		public static void Upsert<T, U>(this Dictionary<T, U> _dictionary, T _key, U _value)
+		{
+			if (!_dictionary.ContainsKey(_key))
+				_dictionary.Add(_key, _value);
+
+			_dictionary[_key] = _value;
+		}
+	}
+}
---
+++ Unity/CodeSnippet/Assets/Editor/RemoveComponent.cs
@@ -0,0 +1,49 @@
+﻿using UnityEditor;
+using UnityEditor.VersionControl;
+using UnityEngine;
+
+namespace aisilol
+{
+	public class RemoveComponentForPrefab
+	{
+		[MenuItem("Assets/Remove All Components For Prefab")]
+		public static void RemoveComponent()
+		{
+			// Remove Component Type
+			var type = typeof(AudioSource);
+
+			var assetPaths = AssetDatabase.GetAllAssetPaths();
+			for (var index = 0; index < assetPaths.Length; index++)
+			{
+				var assetPath = assetPaths[index];
+
+				var title = string.Format("Remove All Components For Prefab ({0}/{1})", index, assetPaths.Length);
+				EditorUtility.DisplayProgressBar(title, assetPath, (float)index / assetPaths.Length);
+
+				if (!assetPath.ToLower().EndsWith("prefab"))
+					continue;
+
+				var asset = AssetDatabase.LoadAssetAtPath<GameObject>(assetPath);
+				if (asset == null)
+					continue;
+
+				var components = asset.GetComponentsInChildren(type);
+				if (components.Length == 0)
+					continue;
+
+				// Check Out
+				Provider.Checkout(asset, CheckoutMode.Exact).Wait();
+
+				foreach (var c in components)
+				{
+					GameObject.DestroyImmediate(c, true);
+				}
+
+				EditorUtility.SetDirty(asset);
+			}
+
+			AssetDatabase.SaveAssets();
+			EditorUtility.ClearProgressBar();
+		}
+	}
+}
