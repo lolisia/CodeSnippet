345b924e3a72747ae4676f8504b72932178be1bb
lolisia <lolisia@gmail.com>
2017-02-20 21:15:36 +0900

    GameObjectExtension::AddComponentOnce 호출 방식 수정

--- Unity/CodeSnippet/Assets/GameObjectExtension.cs
+++ Unity/CodeSnippet/Assets/GameObjectExtension.cs
@@ -2,9 +2,9 @@
 
 namespace aisilol
 {
-	public class GameObjectExtension
+	public static class GameObjectExtension
 	{
-		public static T AddComponentOnce<T>(GameObject _object) where T : Component
+		public static T AddComponentOnce<T>(this GameObject _object) where T : Component
 		{
 			var attachedComponent = _object.GetComponent<T>();
 			return attachedComponent ? attachedComponent : _object.AddComponent<T>();
