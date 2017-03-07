d1704490401b7a3c337dacb6ecafe3af34c19ac4
lolisia <lolisia@gmail.com>
2017-03-08 00:02:18 +0900

    MissingComponentAsset 구현 잘못 올라간 test.cs.meta 삭제

---
+++ Unity/CodeSnippet/Assets/aisilol/Editor/ComponentUtils.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace aisilol
+{
+	public static class ComponentUtils
+	{
+		public static List<GameObject> FindMissingComponentAssets()
+		{
+			// Prefab Preload
+			var result = new List<GameObject>();
+			foreach (var assetPath in AssetDatabase.FindAssets("t:GameObject"))
+			{
+				AssetDatabase.LoadAssetAtPath<GameObject>(assetPath);
+			}
+
+			foreach (var instance in Resources.FindObjectsOfTypeAll<GameObject>())
+			{
+				foreach (var component in instance.GetComponents<Component>())
+				{
+					if (component != null)
+						continue;
+
+					result.Add(instance);
+				}
+			}
+
+			return result;
+		}
+	}
+}
