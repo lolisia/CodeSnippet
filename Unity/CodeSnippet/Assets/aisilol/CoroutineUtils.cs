13c7617d1cd6e7250602924fd85a538ec7172631
lolisia <lolisia@gmail.com>
2017-03-13 22:15:20 +0900

    CoroutineRunner 구현

---
+++ Unity/CodeSnippet/Assets/aisilol/CoroutineUtils.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace aisilol
+{
+	public class CoroutineRunner : MonoBehaviour
+	{
+		public static void Run(IEnumerator _routine)
+		{
+			if (_routine == null)
+				return;
+
+			var instance = new GameObject("[CoroutineRunner]");
+			var runner = instance.AddComponent<CoroutineRunner>();
+			runner.StartCoroutine(onRun(instance, _routine));
+		}
+
+		private static IEnumerator onRun(GameObject _instance, IEnumerator _routine)
+		{
+			yield return _routine;
+			Destroy(_instance);
+		}
+	}
+}
