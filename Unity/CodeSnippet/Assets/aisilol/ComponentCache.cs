4f8c19e1fc224335869d88ac26be07045833a1e5
lolisia <lolisia@gmail.com>
2017-03-20 00:38:57 +0900

    ComponentCache 구현

---
+++ Unity/CodeSnippet/Assets/aisilol/ComponentCache.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace aisilol
+{
+	public class ComponentCache
+	{
+		public ComponentCache(GameObject _parent)
+		{
+			mGameObject = _parent;
+		}
+		public T Get<T>() where T : Component
+		{
+			var name = typeof(T).Name;
+			if (mComponentCacheDic.Find(name))
+			{
+				var value = mComponentCacheDic[name];
+
+				// Cache 대상이 없다면, Cache에서 제거한다.
+				if (value == null)
+					mComponentCacheDic.Remove(name);
+
+				return value as T;
+			}
+
+			var component = mGameObject.GetComponent<T>();
+			if (component == null)
+				return null;
+
+			mComponentCacheDic.Add(name, component);
+			return component;
+		}
+
+
+		private GameObject mGameObject;
+		private Dictionary<string, Component> mComponentCacheDic = new Dictionary<string, Component>();
+	}
+}
