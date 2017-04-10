6cfcf534210c3a10c9653403e98605efc98ad898
lolisia <lolisia@gmail.com>
2017-04-11 00:59:54 +0900

    Editor Coroutine Runner, ScopeProfiler

--- Unity/CodeSnippet/Assets/aisilol/CoroutineUtils.cs
+++ Unity/CodeSnippet/Assets/aisilol/CoroutineUtils.cs
@@ -10,15 +10,91 @@ namespace aisilol
 			if (_routine == null)
 				return;
 
-			var instance = new GameObject("[CoroutineRunner]");
-			var runner = instance.AddComponent<CoroutineRunner>();
-			runner.StartCoroutine(onRun(instance, _routine));
+#if UNITY_EDITOR
+			if (!Application.isPlaying)
+			{
+				new EditorCoroutineRunner(_routine);
+				return;
+			}
+#endif
+			if (sRunner == null)
+			{
+				var instance = new GameObject("[CoroutineRunner]");
+				DontDestroyOnLoad(instance);
+
+				sRunner = instance.AddComponent<CoroutineRunner>();
+			}
+
+			sRunner.StartCoroutine(_routine);
 		}
 
-		private static IEnumerator onRun(GameObject _instance, IEnumerator _routine)
+#if UNITY_EDITOR
+		class EditorCoroutineRunner
 		{
-			yield return _routine;
-			Destroy(_instance);
+			interface IWaitItem { bool Wait(); }
+			class WaitItem_WaitForEndOfFrame : IWaitItem
+			{
+				public bool Wait() { mCalled = !mCalled; return mCalled; }
+				private bool mCalled;
+			}
+			class WaitItem_WaitForSeconds : IWaitItem
+			{
+				public WaitItem_WaitForSeconds(float _duration) { mResumeTime = Time.realtimeSinceStartup + _duration; }
+				public bool Wait() { return Time.realtimeSinceStartup < mResumeTime; }
+				private float mResumeTime;
+			}
+
+			public EditorCoroutineRunner(IEnumerator _routine)
+			{
+				mRoutine = _routine;
+				UnityEditor.EditorApplication.update += update;
+			}
+
+			private void update()
+			{
+				if (mWaitItem != null)
+				{
+					if (mWaitItem.Wait())
+						return;
+
+					mWaitItem = null;
+				}
+
+				// Stop
+				if (!mRoutine.MoveNext())
+				{
+					UnityEditor.EditorApplication.update -= update;
+					return;
+				}
+
+				// WaitForSeconds
+				var waitForSeconds = mRoutine.Current as WaitForSeconds;
+				if (waitForSeconds != null)
+				{
+					var type = waitForSeconds.GetType();
+					var fieldInfo = type.GetField("m_Seconds", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+					if (fieldInfo == null)
+						return;
+
+					var duration = (float)fieldInfo.GetValue(waitForSeconds);
+					mWaitItem = new WaitItem_WaitForSeconds(duration);
+					return;
+				}
+
+				// WaitForEndOfFrame
+				var waitForEndOfFrame = mRoutine.Current as WaitForEndOfFrame;
+				if (waitForEndOfFrame != null)
+				{
+					mWaitItem = new WaitItem_WaitForEndOfFrame();
+					return;
+				}
+			}
+
+			private IEnumerator mRoutine;
+			private IWaitItem mWaitItem;
 		}
+#endif
+
+		private static CoroutineRunner sRunner;
 	}
 }
---
+++ Unity/CodeSnippet/Assets/aisilol/ScopeProfiler/ScopeProfiler.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace aisilol
+{
+	public class ScopeProfiler : Scope
+	{
+		public ScopeProfiler(string _name)
+		{
+			mName = _name;
+			mStartTime = Time.realtimeSinceStartup;
+		}
+		protected override void CloseScope()
+		{
+			ScopeProfilerManager.Add(mName, Time.realtimeSinceStartup - mStartTime);
+		}
+
+
+		private string mName;
+		private float mStartTime;
+	}
+
+	static class ScopeProfilerManager
+	{
+		class ProfileInfo
+		{
+			public int CallCount { get; private set; }
+			public float Elapsed { get; private set; }
+
+			public void AddElapsed(float _elapsed)
+			{
+				CallCount++;
+				Elapsed += _elapsed;
+			}
+
+			public float Average
+			{
+				get
+				{
+					if (CallCount == 0)
+						return Elapsed;
+
+					return MathUtils.Divide(Elapsed, CallCount);
+				}
+			}
+		}
+
+		public static void Add(string _name, float _duration)
+		{
+			var info = sProfileInfoDic.Get(_name);
+			info.AddElapsed(_duration);
+		}
+		public static void Clear()
+		{
+			sProfileInfoDic.Clear();
+		}
+
+		private static Dictionary<string, ProfileInfo> sProfileInfoDic = new Dictionary<string, ProfileInfo>();
+	}
+}
